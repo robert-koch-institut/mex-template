86df00af9c8f19be390b7977eabbf2d546942fa6
Nicolas Drebenstedt <drebenstedtn@rki.de>
2024-03-27 17:53:47 +0100

    Update mex.bat
    Signed-off-by: Nicolas Drebenstedt <drebenstedtn@rki.de>

--- mex-{{ cookiecutter.project_name }}/mex.bat
+++ mex-{{ cookiecutter.project_name }}/mex.bat
@@ -22,7 +22,7 @@ if "%CI%"=="" (
 
 @REM install packages from lock file in local virtual environment
 echo installing package
-pdm sync --clean --group dev
+pdm install -G :all
 exit /b %errorlevel%
 
 
